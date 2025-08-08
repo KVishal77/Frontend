8c71deae6a6f373d4b50002470d4af435aba958a
Vishal <iamvishaljohnson@gmail.com>
2025-08-08 16:03:53 +0530

    Updated API base URL to deployed backend

--- src/api/plantAPI.js
+++ src/api/plantAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "http://plantshazam-backend.dokply.app";
 
 export async function getSuggestion(plantName) {
     const res = await axios.post(`${API_BASE}/suggest`, { plantName });
